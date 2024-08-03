460364cd5efdb62f4c94518b871743c7db1642ed
strigsys <strigsys@outlook.com>
2024-08-03 17:14:42 -0600

    Update vite.config.ts to correct github pages

--- strigsys.company.page/vite.config.ts
+++ strigsys.company.page/vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+// base of github pages
+// in development is root
 export default defineConfig({
   plugins: [react()],
+  base: '/strigsys.company.page/'
 })
