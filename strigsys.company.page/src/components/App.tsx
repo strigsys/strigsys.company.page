4948a7203dfd032f480565b79b055c1c52cf04e8
Javier Andres Guerrero Hernandez <jagh@outlook.com>
2024-08-04 18:04:34 -0600

    [improvement] Use of HashRouter instead BrowserRouter

--- strigsys.company.page/src/components/App.tsx
+++ strigsys.company.page/src/components/App.tsx
@@ -1,5 +1,5 @@
 //import { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import PrivacyPolicy from "./page/PrivacyPolicy/PrivacyPolicy";
 import HomePage from "./page/HomePage/HomePage";
 //import reactLogo from './assets/react.svg';
